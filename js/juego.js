fe1ec289fce669e787f642576b3ef8dfef72f444
Marisol Realivazquez <66392967+Uinose@users.noreply.github.com>
2021-01-05 23:20:32 -0700

    JSON y Arrays en JS

--- js/juego.js
+++ js/juego.js
@@ -1,8 +1,25 @@
 //Me aseguro de que la pagina se ha cargado toda la pagina
-$(document).ready(function(){
+var indiceRandom=0;
+var op=[];
+/*$(document).ready(function(){
     $("#login").animate({
         'top':"10%"
-    },300);
+    },300);*/
+    $.getJSON('https://raw.githubusercontent.com/Bowserinator/Periodic-Table-JSON/master/PeriodicTableJSON.json',
+    function(data){
+       // console.log(data.elements[0].name);
+       var cuantos = data.elements.length;
+       indiceRandom=Math.floor(Math.random()*cuantos);
+       $("#Elemento").find('h2').text(data.elements[indiceRandom].name);
+       for(var i=0; i<3; i++){
+           var n=(Math.floor(Math.random)*cuantos);
+           op.push(n);
+           $("#Opciones").find('ul').append('<li><h2>'+data.elements[n].symbol+'</h2></li>');
+       }
+       $("#Opciones").find('ul').append('<li><h2>'+data.elements[indiceRandom].symbol+'</h2></li>');
+       op.push(indiceRandom);
+       console.log(op);
+    });
     $("#txtNombre").on('keyup',function() {
         $("#txtNombre").css('border','1px solid white');
     });
@@ -19,4 +36,4 @@ $(document).ready(function(){
         }
         
     });
-});
+//});
